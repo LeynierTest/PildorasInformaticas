a10f88ee9923efa991e948229adf952060ba146e
Leynier770 <126425614+LeynierTest@users.noreply.github.com>
2023-03-02 11:34:19 -0500

    Add files via upload

---
+++ Project1/src/Logic/PrimeraClase.java
@@ -0,0 +1,8 @@
+package Logic;
+public class PrimeraClase{
+    public static void main(String[] args) {
+        System.out.println("Leynier");
+        System.out.println("Hola mundo");
+        System.out.println("LLLL");
+    }
+}
